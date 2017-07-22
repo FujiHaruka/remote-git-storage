85ad565ad82c52d069b50ec7c6762372b9e92401
FujiHaruka <fujiharuka@realglobe.jp>
2017-07-22 15:58:13 +0900

    Add Ponfile for tasks

---
+++ Ponfile.js
@@ -0,0 +1,17 @@
+const pon = require('pon')
+const { env, mocha } = require('pon-task-basic')
+
+let TestEnv = {}
+try {
+  TestEnv = require('./TestEnv.json')
+} catch (e) {
+  // Ignore
+}
+
+module.exports = pon({
+  'env:test': env('test', TestEnv),
+  'test': [
+    'env:test',
+    mocha('test/*.test.js')
+  ]
+})
